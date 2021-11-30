8565efc06c7ff91bb87d7e6be606583fdbea42b2
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2021-11-30 18:04:51 +0200

    add preprocessing functions

---
+++ ex2.py
@@ -0,0 +1,43 @@
+import sys
+
+
+
+def init(params):
+    with open(output, 'w') as f:
+        for i in range(6):
+            f.write(f'Output{i + 1}: {params[i]}\n')
+
+
+def development_pre_processing():
+    # take all the articles from the develop file (i%4 == 2 because [0 -> header, 1-> \n ,2->article,3->\n]
+    with open(development, 'r') as f:
+        lines = [l for i, l in enumerate(f.readlines()) if i % 4 == 2]
+
+    # flat the lines to all the words in the file
+    words = [word for line in lines for word in line.split()]
+
+    with open(output, 'a') as f:
+        f.write(f'Output{7}: {len(words)}\n')
+    return words
+
+def separate_validation(words,div = 0.9):
+    train_len = int(len(words) * div)
+    return words[:train_len], words[train_len:]
+
+
+def lidstone_model_training(words):
+    train, validate = separate_validation(words)
+    set_train = set(train)
+    word_count = train.count(word)
+    params = [len(validate),len(train),len(set_train), word_count]
+    with open(output, 'a') as f:
+        for i in range(8,12):
+            f.write(f'Output{i}: {params[i-8]}\n')
+
+
+if __name__ == '__main__':
+    V = 300000
+    development, test, word, output = sys.argv[1:]
+    init([development, test, word, output, V, 1 / V])
+    words = development_pre_processing()
+    lidstone_model_training(words)

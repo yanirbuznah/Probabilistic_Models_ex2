f64c51e5c856761a0748b0a5dc2abfabad972127
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2021-12-02 15:54:06 +0200

    Corrections, improvements and beautification. change to OOP.

--- ex2.py
+++ ex2.py
@@ -1,48 +1,164 @@
+import math
 import sys
+from typing import List
+
+from consts import *
 
 
+class Util:
+    @staticmethod
+    def separate_validation(words, div=0.9):
+        train_len = int(len(words) * div)
+        return words[:train_len], words[train_len:]
 
-def init(params):
-    with open(output, 'w') as f:
-        for i in range(6):
-            f.write(f'Output{i + 1}: {params[i]}\n')
+    @staticmethod
+    def count(text):
+        """
+        :param text: list of words
+        :return: dictionary of words and number of instances per word
+        """
+
+        dct = {}
+        for word in text:
+            if word in dct.keys():
+                dct[word] += 1
+            else:
+                dct[word] = 1
+        return dct
+
+    @staticmethod
+    def development_pre_processing(development_file):
+        """
+
+        :param development_file: development file name
+        :return: list of all the words (events) in the file
+        """
+        # take all the articles from the develop file (i%4 == 2 because [0 -> header, 1-> \n ,2->article,3->\n]
+        with open(development_file, 'r') as f:
+            lines = [l for i, l in enumerate(f.readlines()) if i % 4 == 2]
+
+        # flat the lines to all the words in the file
+        words = [word for line in lines for word in line.split()]
+
+        return words
 
 
-def development_pre_processing():
-    # take all the articles from the develop file (i%4 == 2 because [0 -> header, 1-> \n ,2->article,3->\n]
-    with open(development, 'r') as f:
-        lines = [l for i, l in enumerate(f.readlines()) if i % 4 == 2]
+class OutPut(object):
+    def __init__(self, output_file):
+        self._output_file = output_file
+        self._line_count = 0
+        self._write_names()
 
-    # flat the lines to all the words in the file
-    words = [word for line in lines for word in line.split()]
+    def _write_names(self):
+        with open(self._output_file, 'w') as f:
+            f.write(f"#Students\t{NAMES[0]} {NAMES[1]} {IDS[0]} {IDS[1]}\n")
 
-    with open(output, 'a') as f:
-        f.write(f'Output{7}: {len(words)}\n')
-    return words
-
-def separate_validation(words,div = 0.9):
-    train_len = int(len(words) * div)
-    return words[:train_len], words[train_len:]
+    def write_output(self, outcome):
+        self._line_count += 1
+        with open(self._output_file, 'a') as f:
+            f.write(f"#Output{self._line_count}\t{outcome}\n")
 
 
-def lidstone_model_training(words):
-    train, validate = separate_validation(words)
-    set_train = set(train)
-    word_count = train.count(word)
-    word_mle = word_count/len(train)
-    unseen_mle = 0/len(train)
-    lamda = 0.1
-    word_lidstone = (lamda + word_count)/(lamda*V + len(train))
-    unseen_lidstone = lamda/(lamda*V + len(train))
-    params = [len(validate),len(train),len(set_train), word_count,word_mle,unseen_mle,word_lidstone,unseen_lidstone]
-    with open(output, 'a') as f:
-        for i in range(8,16):
-            f.write(f'Output{i}: {params[i-8]}\n')
+class Lidstone(object):
+    def __init__(self, train: List[str], validate: List[str]):
+        self._train = train
+        self._train_length = len(train)
+        self._validate = validate
+        self._validate_length = len(validate)
+
+    def lidstone_per_word(self, word_count, lamda):
+        return (lamda + word_count) / (lamda * V + self._train_length)
+
+    def perplexity(self, lamda, words_instances):
+        denominator = (lamda * V + self._train_length)
+        log_perplexity = 0
+        for w in self._validate:
+            if w in words_instances.keys():
+                pm = (lamda + words_instances[w]) / denominator
+            else:
+                pm = (lamda / denominator)
+            #if pm != 0:
+            log_perplexity += math.log(pm, 2)
+
+        return 2 ** (-log_perplexity / self._validate_length)
+
+    def lambda_tuning(self,words_instances):
+        lamda = 0
+        best_lamda = 0
+        best_perplexity = float('inf')
+        while lamda <= 2:
+            lamda+=0.01
+            perplexity = self.perplexity(lamda,words_instances)
+            if perplexity < best_perplexity:
+                best_lamda = lamda
+                best_perplexity = perplexity
+
+        return best_lamda, best_perplexity
+
+def init(params, output_manager):
+    # outputs 1 to 6
+    for p in params:
+        output_manager.write_output(p)
+
+
+def main():
+    development, test, input_word, output = sys.argv[1:]
+    output_manager = OutPut(output)
+    # get all the words in the development file
+    words = Util.development_pre_processing(development)
+
+    # separate train and validate
+    train, validate = Util.separate_validation(words)
+    lidstone = Lidstone(train, validate)
+
+    # Output 1-6
+    init([development, test, input_word, output, V, 1 / V], output_manager)
+
+    # Output 7
+    output_manager.write_output(len(validate))
+
+    # Output 8
+    output_manager.write_output(len(train))
+
+    # Output 9
+    output_manager.write_output(len(validate))
+
+    # Output 10
+    train_instances = Util.count(train)
+    output_manager.write_output(len(train_instances))  # number of different events in the training set
+
+    # Output 11
+    word_count = train.count(input_word)
+    output_manager.write_output(word_count)
+
+    # Output 12
+    input_word_mle = word_count / len(train)
+    output_manager.write_output(input_word_mle)
+
+    # Output 13
+    unseen_word_mle = train.count('unseen_word') / len(train)
+    output_manager.write_output(unseen_word_mle)
+
+    # Output 14
+    output_manager.write_output(lidstone.lidstone_per_word(word_count=word_count, lamda=0.1))
+
+    # Output 15
+    output_manager.write_output(lidstone.lidstone_per_word(word_count=train.count('unseen_word'), lamda=0.1))
+
+    # Output 16
+    words_instances = Util.count(train)
+    output_manager.write_output(lidstone.perplexity(lamda=0.01,words_instances=words_instances))
+
+    # Output 17
+    output_manager.write_output(lidstone.perplexity(lamda=0.1,words_instances=words_instances))
+
+    # Output 18
+    output_manager.write_output(lidstone.perplexity(lamda=1.0,words_instances=words_instances))
+
+    # Output 19
+    output_manager.write_output(lidstone.lambda_tuning(words_instances=words_instances)[1])
+
 
 
 if __name__ == '__main__':
-    V = 300000
-    development, test, word, output = sys.argv[1:]
-    init([development, test, word, output, V, 1 / V])
-    words = development_pre_processing()
-    lidstone_model_training(words)
+    main()
